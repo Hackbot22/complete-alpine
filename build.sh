8cd50d2a2c06ee9a76b0e701e7c019b4f2c65fde
Hackbot22 <80085651+Hackbot22@users.noreply.github.com>
2024-12-13 22:15:49 -0700

    made changes

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
 apk add --update-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community
 apk add hyprland
 apk add hyprland-wallpapers
 apk add dpkg
 apk add apt
+apk update
+apt update && apt upgrade
+apt install snapd
+snap install snap-store
+apt update && apt upgrade
 
 hyperland_service_code = "
 [Unit]
@@ -15,5 +22,7 @@ ExecStart=/usr/bin/hyprland
 [Install]
 WantedBy=multi-user.target
 "
-
 echo "$hyperland_service_code" >> /etc/systemd/system/hyprland.service
+systemctl enable hyprland.service
+systemctl start hyprland.service
+reboot
